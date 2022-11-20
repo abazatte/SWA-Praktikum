93452103a37847222cac944f970644db496eddca
TimCirksena <tim.cirksena@outlook.de>
2022-11-20 15:06:43 +0100

    fix: In CocktailResource NutzerIn ind CocktailNutzerIn geändert

--- 2Prak/code-with-quarkus/src/main/java/org/boundary/rest/CocktailResource.java
+++ 2Prak/code-with-quarkus/src/main/java/org/boundary/rest/CocktailResource.java
@@ -58,7 +58,7 @@ public class CocktailResource {
     @GET
     @Operation(summary = "Gets all Cocktails", description = "Lists all available cocktails")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "getAllCount", description = "Wie oft wurde die cocktailList-Methode ausgefuehrt?")
     @Timed(name = "getAllTimer", description = "Wie lange braucht die cocktailList-Methode", unit = MetricUnits.MILLISECONDS)
     public Collection<CocktailDTO> cocktailList(){
@@ -71,7 +71,7 @@ public class CocktailResource {
     @Path("/{id}")
     @Operation(summary = "Gets Cocktail per ID", description = "Lists the Cocktail with same ID as typed")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "getByIDCount", description = "Wie oft wurde die getCocktail-Methode ausgefuehrt?")
     @Timed(name = "getByIDTimer", description = "Wie lange braucht die getCocktail-Methode", unit = MetricUnits.MILLISECONDS)
     public Response getCocktail(@PathParam("id") int id){
@@ -87,7 +87,7 @@ public class CocktailResource {
     @Path("/post")
     @Operation(summary = "Post a CocktailDTO", description = "Creates new Cocktail")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "postCount", description = "Wie oft wurde die newCocktail-Methode ausgefuehrt?")
     @Timed(name = "postTimer", description = "Wie lange braucht die newCocktail-Methode", unit = MetricUnits.MILLISECONDS)
     public Response newCocktail(CocktailDTO cocktailDTO){
@@ -99,7 +99,7 @@ public class CocktailResource {
     @Path("/{id}")
     @Operation(summary = "Delete Cocktail", description = "Delete Cocktail per ID")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "deleteCount", description = "Wie oft wurde die deleteCocktail-Methode ausgefuehrt?")
     @Timed(name = "deleteTimer", description = "Wie lange braucht die deleteCocktail-Methode", unit = MetricUnits.MILLISECONDS)
     public Response deleteCocktail(@PathParam("id") int id){
@@ -116,7 +116,7 @@ public class CocktailResource {
     @Path("/put")
     @Operation(summary = "Edit Cocktail", description = "Edit Cocktail with new name, beschreibung")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "putCount", description = "Wie oft wurde die editCocktail-Methode ausgefuehrt?")
     @Timed(name = "putTimer", description = "Wie lange braucht die editCocktail-Methode", unit = MetricUnits.MILLISECONDS)
     public Response editCocktail(@QueryParam("oldname") String oldname, @QueryParam("newname") String newname, @QueryParam("beschreibung") String beschreibung){
@@ -128,7 +128,7 @@ public class CocktailResource {
     @Path("/{id}")
     @Operation(summary = "Add Zutat", description = "Added new Zutat to given Cocktail per ID")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "patchAddCount", description = "Wie oft wurde die addZutat-Methode ausgefuehrt?")
     @Timed(name = "patchAddTimer", description = "Wie lange braucht die addZutat-Methode", unit = MetricUnits.MILLISECONDS)
     public Response addZutat(@QueryParam("zutat") String zutat, @PathParam("id") int id){
@@ -144,7 +144,7 @@ public class CocktailResource {
     @Path("/deleteZutat/{id}")
     @Operation(summary = "Delete 1 Zutat", description = "Delete Zutat from Cocktail per ID")
     @APIResponses(value = @APIResponse(responseCode = "200", description = "Success",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = NutzerIn.class))))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailNutzerIn.class))))
     @Counted(name = "patchDeleteCount", description = "Wie oft wurde die deleteZutat-Methode ausgefuehrt?")
     @Timed(name = "patchDeleteTimer", description = "Wie lange braucht die deleteZutat-Methode?", unit = MetricUnits.MILLISECONDS)
     public Response deleteZutat(@QueryParam("zutat") String zutat, @PathParam("id") int id){
