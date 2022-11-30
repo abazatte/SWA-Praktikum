2466e5755ab172081e30c521eacc0e8e4de23518
abazatte <92862432+abazatte@users.noreply.github.com>
2022-11-30 14:39:19 +0100

    feat: Interface und Catalog fuer Person angepasst.

--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonInterface.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonInterface.java
@@ -3,9 +3,12 @@ package org.mannschaftssport.control;
 import org.mannschaftssport.boundary.ACL.PlayerDTO;
 import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
 
+import java.util.Collection;
 import java.util.Map;
 
 public interface PersonInterface {
     PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO);
     PlayerDTO addAttributes(long id, Map<String, String> attributes);
+    PlayerDTO getPlayerByID(long id);
+    Collection<PlayerDTO> getAllPlayer();
 }
--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/PersonCatalog.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/PersonCatalog.java
@@ -3,9 +3,12 @@ package org.mannschaftssport.entity;
 import org.mannschaftssport.boundary.ACL.PlayerDTO;
 import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
 
+import java.util.Collection;
 import java.util.Map;
 
 public interface PersonCatalog {
     PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO);
     PlayerDTO addAttributes(long id, Map<String, String> attributes);
+    PlayerDTO getPlayerByID(long id);
+    Collection<PlayerDTO> getAllPlayer();
 }
