4a228258859fceef6f864512f98f921c7c886828
abazatte <92862432+abazatte@users.noreply.github.com>
2022-11-30 15:00:03 +0100

    feat: PersonService ist done.

--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonService.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonService.java
@@ -1,7 +1,32 @@
 package org.mannschaftssport.control;
 
+import org.mannschaftssport.boundary.ACL.PlayerDTO;
+import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
+import org.mannschaftssport.entity.PersonCatalog;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.Map;
 
 @ApplicationScoped
-public class PersonService {
+public class PersonService{
+    @Inject
+    PersonCatalog personRepository;
+
+    public PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO) {
+        return personRepository.addPlayer(playerDTO, spielerpassDTO);
+    }
+
+    public PlayerDTO addAttributes(long id, Map<String, String> attributes) {
+        return personRepository.addAttributes(id, attributes);
+    }
+
+    public PlayerDTO getPlayerByID(long id) {
+        return personRepository.getPlayerByID(id);
+    }
+
+    public Collection<PlayerDTO> getAllPlayer() {
+        return personRepository.getAllPlayer();
+    }
 }
