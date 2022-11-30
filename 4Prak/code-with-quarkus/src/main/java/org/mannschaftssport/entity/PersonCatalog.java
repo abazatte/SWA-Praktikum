4f5f11e97f3389d47243fcc9e463b9ef326a8582
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-11-30 12:24:14 +0100

    feat: dummy implementierung von catalogen

--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/PersonCatalog.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/PersonCatalog.java
@@ -1,4 +1,11 @@
 package org.mannschaftssport.entity;
 
+import org.mannschaftssport.boundary.ACL.PlayerDTO;
+import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
+
+import java.util.Map;
+
 public interface PersonCatalog {
+    PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO);
+    PlayerDTO addAttributes(long id, Map<String, String> attributes);
 }
--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/TeamCatalog.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/entity/TeamCatalog.java
@@ -1,4 +1,19 @@
 package org.mannschaftssport.entity;
 
+import org.mannschaftssport.boundary.ACL.ManagerDTO;
+import org.mannschaftssport.boundary.ACL.PlayerDTO;
+import org.mannschaftssport.boundary.ACL.TeamDTO;
+
+import java.util.Collection;
+import java.util.Map;
+
 public interface TeamCatalog {
+    Collection<TeamDTO> getAllTeams();
+    TeamDTO getTeamByID(long id);
+    TeamDTO createTeam(TeamDTO team);
+    TeamDTO updateTeam(long id, Map<String,String> attribs);
+    Boolean deleteTeamByID(long id);
+    ManagerDTO getManagerFromTeam(long teamId);
+    ManagerDTO setManagerToTeam(long teamId, ManagerDTO managerDTO);
+    Collection<PlayerDTO> setPlayersToTeam(Collection<PlayerDTO> players, long id);
 }
