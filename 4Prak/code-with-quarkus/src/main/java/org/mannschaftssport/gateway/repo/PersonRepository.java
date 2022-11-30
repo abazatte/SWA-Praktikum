07a7e396b2ebf11ca8b7fcff0fbe71f93bdcee49
abazatte <92862432+abazatte@users.noreply.github.com>
2022-11-30 14:53:38 +0100

    feat: PersonRepository ist done.

--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/gateway/repo/PersonRepository.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/gateway/repo/PersonRepository.java
@@ -2,20 +2,46 @@ package org.mannschaftssport.gateway.repo;
 
 import org.mannschaftssport.boundary.ACL.PlayerDTO;
 import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
+import org.mannschaftssport.entity.Person;
 import org.mannschaftssport.entity.PersonCatalog;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class PersonRepository implements PersonCatalog {
+    private final Map<Integer, Person> personMap;
+
+    public PersonRepository(){
+        personMap = new ConcurrentHashMap<>();
+    }
+
     @Override
     public PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO) {
-        return null;
+        this.personMap.put((int) spielerpassDTO.id, new Person(playerDTO));
+        return this.getPlayerByID(spielerpassDTO.id);
     }
 
     @Override
     public PlayerDTO addAttributes(long id, Map<String, String> attributes) {
-        return null;
+        this.personMap.get(id).setAttributes(attributes);
+        return this.getPlayerByID(id);
+    }
+
+    @Override
+    public PlayerDTO getPlayerByID(long id) {
+        return new PlayerDTO(this.personMap.get(id), null);
+    }
+
+    @Override
+    public Collection<PlayerDTO> getAllPlayer() {
+        Collection<PlayerDTO> dtos = new ArrayList<>();
+        for (Person player: personMap.values()) {
+            dtos.add(new PlayerDTO(player, null));
+        }
+        return dtos;
     }
 }
