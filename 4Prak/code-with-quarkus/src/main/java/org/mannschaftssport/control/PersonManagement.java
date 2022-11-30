bc30777eb915df0210bdcbc8655635bdf68f56d3
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-11-30 15:06:05 +0100

    feat: cocmitsahne PersonManagement.java

--- 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonManagement.java
+++ 4Prak/code-with-quarkus/src/main/java/org/mannschaftssport/control/PersonManagement.java
@@ -4,17 +4,33 @@ import org.mannschaftssport.boundary.ACL.PlayerDTO;
 import org.mannschaftssport.boundary.ACL.SpielerpassDTO;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Collection;
 import java.util.Map;
 
 @ApplicationScoped
 public class PersonManagement implements PersonInterface{
+    @Inject
+    PersonService personService;
+
+
     @Override
     public PlayerDTO addPlayer(PlayerDTO playerDTO, SpielerpassDTO spielerpassDTO) {
-        return null;
+        return personService.addPlayer(playerDTO,spielerpassDTO);
     }
 
     @Override
     public PlayerDTO addAttributes(long id, Map<String, String> attributes) {
-        return null;
+        return personService.addAttributes(id,attributes);
+    }
+
+    @Override
+    public PlayerDTO getPlayerByID(long id) {
+        return personService.getPlayerByID(id);
+    }
+
+    @Override
+    public Collection<PlayerDTO> getAllPlayer() {
+        return personService.getAllPlayer();
     }
 }
