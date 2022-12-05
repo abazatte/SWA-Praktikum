0c1523704ee10b2c362a5ca0dd4e9f471cb386c5
abazatte <92862432+abazatte@users.noreply.github.com>
2022-12-05 12:02:52 +0100

    BeispielProjekt

--- 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/flottenmanagement/boundary/rest/SchiffRessource.java
+++ 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/flottenmanagement/boundary/rest/SchiffRessource.java
@@ -1,4 +1,23 @@
 package org.acme.hibernate.orm.flottenmanagement.boundary.rest;
 
+import org.acme.hibernate.orm.flottenmanagement.boundary.acl.ReturnSchiffDTO;
+import org.acme.hibernate.orm.flottenmanagement.entity.Schiff;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Collection;
+
+@Path("/schiff")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class SchiffRessource {
+
+    @GET
+    public Collection<ReturnSchiffDTO> getAll(){
+        return null;
+    }
 }
--- 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/flottenmanagement/control/Schiffinterface.java
+++ 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/flottenmanagement/control/Schiffinterface.java
@@ -4,7 +4,10 @@ import org.acme.hibernate.orm.flottenmanagement.boundary.acl.DeleteSchiffDTO;
 import org.acme.hibernate.orm.flottenmanagement.boundary.acl.PostSchiffDTO;
 import org.acme.hibernate.orm.flottenmanagement.boundary.acl.ReturnSchiffDTO;
 
+import java.util.Collection;
+
 public interface Schiffinterface {
+    Collection<ReturnSchiffDTO> getAll();
     ReturnSchiffDTO AuftragAnSchiffUebergeben(long id);
     ReturnSchiffDTO addSchiff(PostSchiffDTO postSchiffDTO);
     boolean deleteSchiff(DeleteSchiffDTO deleteSchiffDTO);
