69d8a8adb243e8d7d687d7ddcabfa66ad4174b9c
TimCirksena <tim.cirksena@outlook.de>
2022-12-06 13:57:05 +0100

    feat: Control von Auftrag

--- 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/auftragsmanagement/control/AuftragsService.java
+++ 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/auftragsmanagement/control/AuftragsService.java
@@ -1,4 +1,37 @@
 package org.acme.hibernate.orm.auftragsmanagement.control;
 
-public class AuftragsService {
+import org.acme.hibernate.orm.auftragsmanagement.boundary.acl.PatchAuftragDTO;
+import org.acme.hibernate.orm.auftragsmanagement.boundary.acl.PostAuftragDTO;
+import org.acme.hibernate.orm.auftragsmanagement.boundary.acl.ReturnAuftragDTO;
+import org.acme.hibernate.orm.auftragsmanagement.entity.AuftragsCatalog;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Collection;
+
+@ApplicationScoped
+public class AuftragsService{
+    @Inject
+    AuftragsCatalog auftragsRepository;
+
+
+    public Collection<ReturnAuftragDTO> getAllAuftraege() {
+        return auftragsRepository.getAllAuftraege();
+    }
+
+    public ReturnAuftragDTO addAuftrag(PostAuftragDTO postAuftragDTO) {
+        return auftragsRepository.addAuftrag(postAuftragDTO);
+    }
+
+    public ReturnAuftragDTO editAuftrag(PatchAuftragDTO patchAuftragDTO) {
+        return auftragsRepository.editAuftrag(patchAuftragDTO);
+    }
+
+    public Boolean deleteAuftrag(long id) {
+        return auftragsRepository.deleteAuftrag(id);
+    }
+
+    public ReturnAuftragDTO findAuftragByID(long id) {
+        return auftragsRepository.findAuftragByID(id);
+    }
 }
