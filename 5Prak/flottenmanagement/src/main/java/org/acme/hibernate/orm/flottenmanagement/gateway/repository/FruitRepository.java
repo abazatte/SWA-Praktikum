cd31e44b57decf04f1da943f1137ff8526985002
TimCirksena <tim.cirksena@outlook.de>
2022-12-07 14:16:37 +0100

    feat: fruit rausgelöscht

--- 5Prak/flottenmanagement/src/main/java/org/acme/hibernate/orm/flottenmanagement/gateway/repository/FruitRepository.java
+++
@@ -1,9 +0,0 @@
-package org.acme.hibernate.orm.flottenmanagement.gateway.repository;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class FruitRepository implements PanacheRepository<Fruit> {
-}
