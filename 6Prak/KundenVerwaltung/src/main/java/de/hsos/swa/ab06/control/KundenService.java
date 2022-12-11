1575b8dfb291eff1bf944d253f37a6ed65ff76ae
abazatte <a.azattemuer@gmail.com>
2022-12-11 15:47:08 +0100

    feat: KundenService hat jetzt die Methoden ausgeproggt.

--- 6Prak/KundenVerwaltung/src/main/java/de/hsos/swa/ab06/control/KundenService.java
+++ 6Prak/KundenVerwaltung/src/main/java/de/hsos/swa/ab06/control/KundenService.java
@@ -3,39 +3,82 @@ package de.hsos.swa.ab06.control;
 import de.hsos.swa.ab06.boundary.rs.AdresseDTO;
 import de.hsos.swa.ab06.boundary.rs.PostKundeDTO;
 import de.hsos.swa.ab06.boundary.rs.ReturnKundeDTO;
+import de.hsos.swa.ab06.entity.Adresse;
+import de.hsos.swa.ab06.entity.Kunde;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
+@ApplicationScoped
 public class KundenService {
+    private Map<Long, Kunde> kundeMap = new ConcurrentHashMap<>();
+    AtomicLong idGen = new AtomicLong();
+
+    private Long createID(){
+        return idGen.getAndIncrement();
+    }
     public ReturnKundeDTO kundeAnlegen(PostKundeDTO postKundeDTO) {
-        return null;
+        Kunde kunde = new Kunde();
+        kunde.setVorname(postKundeDTO.vorname);
+        kunde.setNachname(postKundeDTO.nachname);
+        kunde.setKundenNummer(createID());
+        kundeMap.put(kunde.getKundenNummer(), kunde);
+        return kundeAbfragen(kunde.getKundenNummer());
     }
 
     public Collection<ReturnKundeDTO> kundenAbfragen() {
-        return null;
+        Collection<Kunde> kundeCollection = kundeMap.values();
+        Collection<ReturnKundeDTO> returnKundeDTOS = new ArrayList<>();
+        for (Kunde kunde: kundeCollection) {
+            returnKundeDTOS.add(new ReturnKundeDTO(kunde));
+        }
+        return returnKundeDTOS;
     }
 
     public ReturnKundeDTO kundeAbfragen(long kundenNr) {
-        return null;
+        return new ReturnKundeDTO(kundeMap.get(kundenNr));
     }
 
     public boolean kundeLoeschen(long kundenNr) {
-        return false;
+        Kunde kunde = kundeMap.remove(kundenNr);
+        return kunde != null;
     }
 
     public AdresseDTO adresseAnlegen(long kundenNr, AdresseDTO adr) {
-        return null;
+        Adresse adresse = new Adresse();
+        adresse.setHausnr(adr.hausnr);
+        adresse.setOrt(adr.ort);
+        adresse.setPlz(adr.plz);
+        adresse.setStrasse(adr.strasse);
+        Kunde kunde = kundeMap.get(kundenNr);
+        kunde.setAdresse(adresse);
+        kundeMap.replace(kundenNr, kunde);
+        return adr;
     }
 
     public AdresseDTO adresseAendern(long kundenNr, AdresseDTO neueAdr) {
-        return null;
+        Adresse adresse = new Adresse();
+        adresse.setHausnr(neueAdr.hausnr);
+        adresse.setOrt(neueAdr.ort);
+        adresse.setPlz(neueAdr.plz);
+        adresse.setStrasse(neueAdr.strasse);
+        Kunde kunde = kundeMap.get(kundenNr);
+        kunde.setAdresse(adresse);
+        kundeMap.replace(kundenNr, kunde);
+        return neueAdr;
     }
 
     public AdresseDTO adresseAbfragen(long kundenNr) {
-        return null;
+        Kunde kunde = kundeMap.get(kundenNr);
+        return new AdresseDTO(kunde.getAdresse());
     }
 
     public boolean adresseLoeschen(long kundenNr) {
-        return false;
+        kundeMap.get(kundenNr).setAdresse(null);
+        return kundeMap.get(kundenNr) == null;
     }
 }
