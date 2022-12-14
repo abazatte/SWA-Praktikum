508a508953eae3d67387feaae05a6810e2d2aaf7
abazatte <a.azattemuer@gmail.com>
2022-12-14 12:04:19 +0100

    feat: Katalog ist done.

--- 6Prak/KundenVerwaltung/src/main/java/de/hsos/swa/ab06pers/control/KundenCatalog.java
+++ 6Prak/KundenVerwaltung/src/main/java/de/hsos/swa/ab06pers/control/KundenCatalog.java
@@ -6,5 +6,11 @@ import java.util.Collection;
 
 public interface KundenCatalog {
     Collection<ReturnKundePersistentDTO> kundenAbfragen();
-
+    ReturnKundePersistentDTO kundeAnlegen(PostKundePersistentDTO postKundePersistentDTO);
+    ReturnKundePersistentDTO kundeAbfragen(long kundenNr);
+    boolean kundeLoeschen(long kundenNr);
+    AdressePersistentDTO adresseAnlegen(long kundenNr, AdressePersistentDTO adr);
+    AdressePersistentDTO adresseAendern(long kundenNr, AdressePersistentDTO neueAdr);
+    adressePersistentDTO adresseAbfragen(long kundenNr);
+    boolean adresseLoeschen(long kundenNr);
 }
